716caba359d63ec56c722be96b91ef9904392464
amirhoseinzarei <amirhoseinzarei8484@gmail.com>
2024-11-04 23:46:30 +0330

    Add files via upload
    تمرین 5 تنها کاراکتر دریافت می کندc

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("لطفاً یک کاراکتر وارد کنید:");
+
+        // دریافت کاراکتر
+        char character = (char)Console.Read();
+
+        // نمایش کاراکتر دریافتی
+        Console.WriteLine($"کاراکتر دریافتی: {character}");
+    }
+}
